2564af3f300e43bac57104f6098d298ba6d2709a
NormandoJefferson <normandojefferson@hotmail.com>
2025-01-04 10:02:27 -0300

    chore: create ServiceImpl classes folder

--- src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -1,7 +1,8 @@
-package com.ead.course.services;
+package com.ead.course.services.impl;
 
 import com.ead.course.models.LessonModel;
 import com.ead.course.repositories.LessonRepository;
+import com.ead.course.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,9 +1,10 @@
-package com.ead.course.services;
+package com.ead.course.services.impl;
 
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
+import com.ead.course.services.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
